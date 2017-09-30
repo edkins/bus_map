a00b194bbac780603823a8f61f94744060c54051
Giles Edkins <edkins@gmail.com>
2017-09-29 22:52:33 -0400

    Refactor linelet discovery

--- setup/find_paths.py
+++ setup/find_paths.py
@@ -1,26 +1,65 @@
 import sys
 import json
 
-shapes = json.load(open(sys.argv[1]))
-
 class Linelet:
-    def __init__(self, point0, point1):
-        if (point0['lat'], point0['lon']) < (point1['lat'], point1['lon']):
-            self.lat0 = point0['lat']
-            self.lon0 = point0['lon']
-            self.lat1 = point1['lat']
-            self.lon1 = point1['lon']
-        else:
-            self.lat1 = point0['lat']
-            self.lon1 = point0['lon']
-            self.lat0 = point1['lat']
-            self.lon0 = point1['lon']
-
-    def str(self):
-        return str(self.lat0)+','+str(self.lon0)+','+str(self.lat1)+','+str(self.lon1)
+    def __init__(self, lat0, lon0, lat1, lon1):
+        self.lat0 = lat0
+        self.lon0 = lon0
+        self.lat1 = lat1
+        self.lon1 = lon1
 
     def shape(self):
         return {'points':[{'lat':self.lat0,'lon':self.lon0},{'lat':self.lat1,'lon':self.lon1}]}
+
+    def reverse(self):
+        return Linelet(self.lat1, self.lon1, self.lat0, self.lon0)
+
+    def __eq__(self, other):
+        return self.lat0 == other.lat0 and self.lon0 == other.lon0 and self.lat1 == other.lat1 and self.lon1 == other.lon1
+    def __ne__(self, other):
+        return not (self == other)
+    def __hash__(self):
+        return hash((self.lat0,self.lon0,self.lat1,self.lon1))
+
+class RoutePoint:
+    def __init__(self, route, i):
+        self.route = route
+        self.i = i
+
+    def __eq__(self, other):
+        return self.route == other.route and self.i == other.i
+    def __ne__(self, other):
+        return not (self == other)
+    def __hash__(self):
+        return hash((self.route,self.i))
+
+class RouteSet:
+    def __init__(self):
+        self.set = set()
+
+    def add(self, route, i):
+        self.set.add(RoutePoint(route, i))
+
+class LineletInfo:
+    def __init__(self):
+        self.info = {}
+
+    def add_linelet(self, route, i, point0, point1):
+        linelet = Linelet(point0['lat'], point0['lon'], point1['lat'], point1['lon'])
+        if linelet not in self.info:
+            self.info[linelet] = RouteSet()
+        self.info[linelet].add(route,i)
+
+    def add_shape(self, shape):
+        route = shape['id']
+        points = shape['points']
+        length = len(points)-1
+        for i in range(length):
+            self.add_linelet(route, i, points[i], points[i+1])
+            self.add_linelet(route,-1-i, points[length-i], points[length-i-1])
+
+    def linelets(self):
+        return self.info.keys()
 
 def pairs(list):
     result = []
@@ -28,14 +67,20 @@ def pairs(list):
         result.append((list[i],list[i+1]))
     return result
 
-linelets = {}
-for shape in shapes:
-    for (point0,point1) in pairs(shape['points']):
-        linelet = Linelet(point0,point1)
-        linelets[linelet.str()] = linelet
+def main():
+    shapes = json.load(open(sys.argv[1]))
+    info = LineletInfo()
+    for shape in shapes:
+        info.add_shape(shape)
 
-paths = []
-for key in linelets:
-    paths.append(linelets[key].shape())
+    paths = []
+    encountered = set()
+    for key in info.linelets():
+        if key not in encountered:
+            paths.append(key.shape())
+            encountered.add(key.reverse())
 
-json.dump(paths, sys.stdout)
+    json.dump(paths, sys.stdout)
+
+main()
+
--- setup/mk_svg.py
+++ setup/mk_svg.py
@@ -17,7 +17,7 @@ class Bounds:
         x2 = x - 0.35 * y
         y2 = y + 0.35 * x
 
-        return Point(200 + 1000 * x2, 1000 * y2 - 300)
+        return Point(400 + 2000 * x2, 2000 * y2 - 600)
 
 class Point:
     def __init__(self, x, y):
@@ -28,14 +28,14 @@ class Point:
         return str(self.x) + ',' + str(self.y)
 
 print('<?xml version="1.0" encoding="UTF-8" ?>')
-print('<svg xmlns="http://www.w3.org/2000/svg" version="1.1">')
+print('<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="2300" height="1500">')
 
 bounds = Bounds()
 for shape in shapes:
     string = ''
     for point in shape['points']:
         string += bounds.scale(point).str() + ' '
-    print('<polyline fill="none" stroke="rgba(0,0,0,0.2)" stroke-width="3" points="%s"/>' % string)
+    print('<polyline fill="none" stroke="rgba(0,0,0,0.2)" stroke-width="1" points="%s"/>' % string)
 
 print('</svg>')
 
